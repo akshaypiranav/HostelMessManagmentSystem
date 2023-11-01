ee19ed2242afd08ddf3925f551a1b1411f5eb1b6
akshaypiranav <akshaypiranavb@gmail.com>
2023-11-01 12:13:11 +0530

    USER DETAILS CREATED

--- APP/views.py
+++ APP/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def index(request):
     return render(request,"index.html")
 
 
 def add(request):
+    if request.method=="POST":
+        studentDB=StudentDetails()
+        name=request.POST.get("name")
+        rollNumber=request.POST.get("rollNumber")
+        mail=request.POST.get("mail")
+        phoneNumber=request.POST.get("phoneNumber")
+        parentPhoneNumber=request.POST.get("parentPhoneNumber")
+        foodType=request.POST.get("foodType")
+
+        studentDB.studentName=name
+        studentDB.studentRollNumber=rollNumber
+        studentDB.studentMail=mail
+        studentDB.studentPhoneNumber=phoneNumber
+        studentDB.parentPhoneNumber=parentPhoneNumber
+        studentDB.foodType=foodType
+
+        if(len(request.FILES)!=0):
+                studentDB.image=request.FILES['image']
+                studentDB.save()
+        else:
+                print("nope")  
     return render(request,"addStudent.html")
 
 
@@ -14,4 +35,5 @@ def updateStudent(request):
 
 
 def search(request):
-    return render(request,"studentDetails.html")
+    studentDB=StudentDetails.objects.all()
+    return render(request,"studentDetails.html",{"studentDB":studentDB})
