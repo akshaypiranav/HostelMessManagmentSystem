384022831a0e3439f7974242aa9c6c6023eacb1e
akshaypiranav <akshaypiranavb@gmail.com>
2023-11-01 14:28:23 +0530

    FINISHED BACKEND SUCCESSFULLY

--- APP/views.py
+++ APP/views.py
@@ -2,12 +2,53 @@ from django.shortcuts import render,redirect
 from .models import *
 # Create your views here.
 def index(request):
+    if request.method=="POST":
+        rollNumber=request.POST.get("rollNumber")
+        if(StudentDetails.objects.filter(studentRollNumber=rollNumber).exists()):
+            studentData=StudentDetails.objects.get(studentRollNumber=rollNumber)
+            print(studentData.image)
+            print("HOSTELLER")
+            return render(request,"index.html",{"studentData":studentData})
+        else:
+            print("NO")
+
     return render(request,"index.html")
+
 
 
 def add(request):
     if request.method=="POST":
         studentDB=StudentDetails()
+        name=request.POST.get("name")
+        rollNumber=request.POST.get("rollNumber")
+        mail=request.POST.get("mail")
+        phoneNumber=request.POST.get("phoneNumber")
+        parentPhoneNumber=request.POST.get("parentPhoneNumber")
+        foodType=request.POST.get("foodType")
+        if(not(StudentDetails.objects.filter(studentRollNumber=rollNumber).exists())):
+            studentDB.studentName=name
+            studentDB.studentRollNumber=rollNumber
+            studentDB.studentMail=mail
+            studentDB.studentPhoneNumber=phoneNumber
+            studentDB.parentPhoneNumber=parentPhoneNumber
+            studentDB.foodType=foodType
+
+            if(len(request.FILES)!=0):
+                    studentDB.image=request.FILES['image']
+                    studentDB.save()
+            else:
+                    print("nope")  
+        else:
+             pass
+    return render(request,"addStudent.html")
+
+
+def updateStudent(request,id):
+    if(StudentDetails.objects.filter(id=id).exists()):
+        studentDB=StudentDetails.objects.get(id=id)
+
+    if request.method=="POST":
+        studentDB=StudentDetails.objects.get(id=id)
         name=request.POST.get("name")
         rollNumber=request.POST.get("rollNumber")
         mail=request.POST.get("mail")
@@ -24,14 +65,12 @@ def add(request):
 
         if(len(request.FILES)!=0):
                 studentDB.image=request.FILES['image']
-                studentDB.save()
+                
         else:
                 print("nope")  
-    return render(request,"addStudent.html")
-
-
-def updateStudent(request,id):
-    return render(request,"updateStudent.html")
+        studentDB.save()
+        return redirect("search")
+    return render(request,"updateStudent.html",{"data":studentDB})
 
 
 def search(request):
@@ -39,4 +78,7 @@ def search(request):
     return render(request,"studentDetails.html",{"studentDB":studentDB})
 
 def delete(request,id):
+     if(StudentDetails.objects.filter(id=id).exists()):
+          studentDB=StudentDetails.objects.get(id=id)
+          studentDB.delete()
      return redirect("search")
--- HostelMessManagmentSystem/urls.py
+++ HostelMessManagmentSystem/urls.py
@@ -17,11 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from APP import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",views.index,name="index"),
     path("add",views.add,name="add"),
     path("updateStudent/<int:id>",views.updateStudent,name="updateStudent"),
     path("search",views.search,name="search"),
-    path("delete/<int:id>",views.delete,name="delete")
+    path("delete/<int:id>",views.delete,name="delete"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
